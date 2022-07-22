defaef8fefa2eaeb7484effe436b81157b040d23
CarlosCr18 <carlosLcrespo18@gmail.com>
2022-07-22 10:42:57 -0500

    Added animations and completed the about me page

--- src/routes/about/About.tsx
+++ src/routes/about/About.tsx
@@ -1,9 +1,109 @@
-import React from "react";
+import { useState } from "react";
 import styles from "./About.module.css";
 import NavBar from "../../reusableComponents/navigationBar/navigationBar";
 import ScrollContainer from "react-indiana-drag-scroll";
+import AboutSkills from "./aboutSkills";
 
 const About = () => {
+	const [showMouse, setShowMouse] = useState(true);
+	const skillsData = [
+		{
+			title: "Front End",
+			skills: [
+				{
+					src: "img/javascript.svg",
+					alt: "Javascript Icon",
+					title: "1.5 years of experience",
+				},
+				{
+					src: "img/html.webp",
+					alt: "HTML Icon",
+					title: "1.5 years of experience",
+				},
+				{
+					src: "img/CSS3.svg",
+					alt: "CSS Icon",
+					title: "1.5 years of experience",
+				},
+				{
+					src: "img/typescript.svg",
+					alt: "Typescript Icon",
+					title: "less than 1 year of experience",
+				},
+				{
+					src: "img/react.webp",
+					alt: "React Icon",
+					title: "1.5 years of experience",
+				},
+				{
+					src: "img/redux.svg",
+					alt: "Redux Icon",
+					title: "1.5 years of experience",
+				},
+				{
+					src: "img/vue.svg",
+					alt: "Vue Icon",
+					title: "less than 1 year of experience",
+				},
+			],
+		},
+		{
+			title: "Back End",
+			skills: [
+				{
+					src: "img/nodejs.svg",
+					alt: "NodeJS Icon",
+					title: "less than 1 year of experience",
+				},
+				{
+					src: "img/express.png",
+					alt: "Express Icon",
+					title: "less than 1 year of experience",
+				},
+				{
+					src: "img/next.svg",
+					alt: "Next Icon",
+					title: "less than 1 year of experience",
+				},
+				{
+					src: "img/mongo.svg",
+					alt: "MongoDB Icon",
+					title: "less than 1 year of experience",
+				},
+				{
+					src: "img/php.svg",
+					alt: "PHP Icon",
+					title: "less than 1 year of experience",
+				},
+				{
+					src: "img/postgresql.svg",
+					alt: "Postgresql Icon",
+					title: "less than 1 year of experience",
+				},
+			],
+		},
+		{
+			title: "Others",
+			skills: [
+				{
+					src: "img/git.svg",
+					alt: "Git Icon",
+					title:
+						"GitHub 1.5 years of experience, GitLab less than 1 year of experience.",
+				},
+				{
+					src: "img/svn.svg",
+					alt: "SVN Icon",
+					title: "Less than 1 year of experience",
+				},
+				{
+					src: "img/python.svg",
+					alt: "Python Icon",
+					title: "Less than 1 year of experience",
+				},
+			],
+		},
+	];
 	return (
 		<div className={styles.About}>
 			<NavBar></NavBar>
@@ -18,148 +118,25 @@ const About = () => {
 					</p>
 				</div>
 
-				<ScrollContainer className={styles.aboutSkills}>
-					<div>
-						<h3>Front End</h3>
-						<div className={styles.aboutSkillsList}>
-							<img
-								loading="lazy"
-								src="img/html.webp"
-								alt="html icon"
-								decoding="async"
-							/>
-							<img
-								loading="lazy"
-								src="img/javascript.svg"
-								alt="javascript icon"
-								decoding="async"
-							/>
-							<img
-								loading="lazy"
-								src="img/CSS3.svg"
-								alt="css icon"
-								decoding="async"
-							/>
-							<div>
-								<img
-									loading="lazy"
-									src="img/react.webp"
-									alt="React icon"
-									decoding="async"
-								/>
-								<p>React</p>
-							</div>
-							<div>
-								<img
-									loading="lazy"
-									src="img/github.svg"
-									alt="Github icon"
-									decoding="async"
-								/>
-								<p>Github</p>
-							</div>
-							<img
-								loading="lazy"
-								src="img/next.svg"
-								alt="next icon"
-								decoding="async"
-							/>
-						</div>
-					</div>
-					<div>
-						<h3>Back End</h3>
-						<div className={styles.aboutSkillsList}>
-							<img
-								loading="lazy"
-								src="img/html.webp"
-								alt="html icon"
-								decoding="async"
-							/>
-							<img
-								loading="lazy"
-								src="img/javascript.svg"
-								alt="javascript icon"
-								decoding="async"
-							/>
-							<img
-								loading="lazy"
-								src="img/CSS3.svg"
-								alt="css icon"
-								decoding="async"
-							/>
-							<div>
-								<img
-									loading="lazy"
-									src="img/react.webp"
-									alt="React icon"
-									decoding="async"
-								/>
-								<p>React</p>
-							</div>
-							<div>
-								<img
-									loading="lazy"
-									src="img/github.svg"
-									alt="Github icon"
-									decoding="async"
-								/>
-								<p>Github</p>
-							</div>
-							<img
-								loading="lazy"
-								src="img/next.svg"
-								alt="next icon"
-								decoding="async"
-							/>
-						</div>
-					</div>
-					<div>
-						<h3>Others</h3>
-						<div className={styles.aboutSkillsList}>
-							<img
-								loading="lazy"
-								src="img/html.webp"
-								alt="html icon"
-								decoding="async"
-							/>
-							<img
-								loading="lazy"
-								src="img/javascript.svg"
-								alt="javascript icon"
-								decoding="async"
-							/>
-							<img
-								loading="lazy"
-								src="img/CSS3.svg"
-								alt="css icon"
-								decoding="async"
-							/>
-							<div>
-								<img
-									loading="lazy"
-									src="img/react.webp"
-									alt="React icon"
-									decoding="async"
-								/>
-								<p>React</p>
-							</div>
-							<div>
-								<img
-									loading="lazy"
-									src="img/github.svg"
-									alt="Github icon"
-									decoding="async"
-								/>
-								<p>Github</p>
-							</div>
-							<img
-								loading="lazy"
-								src="img/next.svg"
-								alt="next icon"
-								decoding="async"
-							/>
-						</div>
-					</div>
+				<ScrollContainer
+					className={styles.aboutSkills}
+					onStartScroll={() => setShowMouse(false)}
+				>
+					<img
+						className={
+							showMouse
+								? `${styles.mouseScroll}`
+								: `${styles.mouseScroll} ${styles.invisible}`
+						}
+						src="/img/scroll.png"
+						alt="Scroll"
+					/>
+					{skillsData.map((sk, index) => (
+						<AboutSkills
+							key={sk.title + "_" + index}
+							skillsData={sk}
+						></AboutSkills>
+					))}
 				</ScrollContainer>
 			</div>
 		</div>
