c0693b0a3737651d17972b46d53d8ca2edf1842c
CarlosCr18 <carlosLcrespo18@gmail.com>
2022-07-25 14:12:13 -0500

    Updated projects

--- src/routes/projects/Projects.tsx
+++ src/routes/projects/Projects.tsx
@@ -9,6 +9,102 @@ const Projects = () => {
 		text: "Here I show you a small list of projects that I've developed, some are more complex than the others or more aesthetic but I've learned so much developing each one of them, hope you enjoy them and any feedback is well welcomed.",
 		projects: [
 			{
+				id: "Ecommerce Front",
+				title: "Small Ecommerce Front",
+				img: "https://crespoportfolioimages.s3.us-east-2.amazonaws.com/smallecommfront.webp",
+				imgAlt: "project Small Ecommerce preview image",
+				liveUrl: "https://small-ecommerce-frontend.netlify.app",
+				github: "https://github.com/CarlosCr18/small-ecommerce",
+				text: "Small Ecommerce functional front end",
+				skills: [
+					{
+						url: "https://crespoportfolioimages.s3.us-east-2.amazonaws.com/javascript.svg",
+						alt: "Javascript",
+					},
+					{
+						url: "https://crespoportfolioimages.s3.us-east-2.amazonaws.com/CSS3.svg",
+						alt: "CSS3",
+					},
+					{
+						url: "https://crespoportfolioimages.s3.us-east-2.amazonaws.com/react.webp",
+						alt: "React",
+					},
+					{
+						url: "https://crespoportfolioimages.s3.us-east-2.amazonaws.com/redux.svg",
+						alt: "Redux",
+					},
+				],
+			},
+			{
+				id: "Ecommerce Back",
+				title: "Small Ecommerce back end",
+				img: "https://crespoportfolioimages.s3.us-east-2.amazonaws.com/smallecommback.webp",
+				imgAlt: "",
+				liveUrl: "https://small-ecommerce-backend.herokuapp.com/",
+				github: "https://github.com/CarlosCr18/small-ecommerce-backend",
+				text: "A backend that handles the small Ecommerce",
+				skills: [
+					{
+						url: "https://crespoportfolioimages.s3.us-east-2.amazonaws.com/nodejs.svg",
+						alt: "NodeJS",
+					},
+					{
+						url: "https://crespoportfolioimages.s3.us-east-2.amazonaws.com/express.png",
+						alt: "Express",
+					},
+				],
+			},
+			{
+				id: "NiponABC",
+				title: "NiponABC",
+				img: "https://crespoportfolioimages.s3.us-east-2.amazonaws.com/niponabc.webp",
+				imgAlt: "project niponabc preview image",
+				liveUrl: "https://niponabc.netlify.app/",
+				github: "https://github.com/CarlosCr18/niponABC",
+				text: "Learn and study the japanese alphabet playing",
+				skills: [
+					{
+						url: "https://crespoportfolioimages.s3.us-east-2.amazonaws.com/javascript.svg",
+						alt: "Javascript",
+					},
+					{
+						url: "https://crespoportfolioimages.s3.us-east-2.amazonaws.com/CSS3.svg",
+						alt: "CSS3",
+					},
+					{
+						url: "https://crespoportfolioimages.s3.us-east-2.amazonaws.com/react.webp",
+						alt: "React",
+					},
+					{
+						url: "https://crespoportfolioimages.s3.us-east-2.amazonaws.com/next.svg",
+						alt: "Next Js",
+					},
+				],
+			},
+			{
+				id: "japaneseApi",
+				title: "Japanese alphabet api",
+				img: "https://crespoportfolioimages.s3.us-east-2.amazonaws.com/apiproject.webp",
+				imgAlt: "Japanese alphabet api preview image",
+				liveUrl: "https://japanese-alphabet-api.herokuapp.com/",
+				github: "https://github.com/CarlosCr18/japAlphabetAPI",
+				text: "An api that responds with the japanese alphabet",
+				skills: [
+					{
+						url: "https://crespoportfolioimages.s3.us-east-2.amazonaws.com/nodejs.svg",
+						alt: "NodeJS",
+					},
+					{
+						url: "https://crespoportfolioimages.s3.us-east-2.amazonaws.com/express.png",
+						alt: "Express",
+					},
+					{
+						url: "https://crespoportfolioimages.s3.us-east-2.amazonaws.com/mongo.svg",
+						alt: "Mongo",
+					},
+				],
+			},
+			{
 				id: "Maskottchen",
 				title: "Dog grooming center",
 				img: "https://crespoportfolioimages.s3.us-east-2.amazonaws.com/maskottchen.webp",
@@ -32,56 +128,6 @@ const Projects = () => {
 					{
 						url: "https://crespoportfolioimages.s3.us-east-2.amazonaws.com/react.webp",
 						alt: "React",
-					},
-				],
-			},
-			{
-				id: "japaneseApi",
-				title: "Japanese alphabet api",
-				img: "https://crespoportfolioimages.s3.us-east-2.amazonaws.com/apiproject.webp",
-				imgAlt: "Japanese alphabet api preview image",
-				liveUrl: "https://japanese-alphabet-api.herokuapp.com/",
-				github: "https://github.com/CarlosCr18/japAlphabetAPI",
-				text: "An api that responds with the japanese alphabet",
-				skills: [
-					{
-						url: "https://crespoportfolioimages.s3.us-east-2.amazonaws.com/nodejs.svg",
-						alt: "Javascript",
-					},
-					{
-						url: "https://crespoportfolioimages.s3.us-east-2.amazonaws.com/express.png",
-						alt: "CSS3",
-					},
-					{
-						url: "https://crespoportfolioimages.s3.us-east-2.amazonaws.com/mongo.svg",
-						alt: "React",
-					},
-				],
-			},
-			{
-				id: "NiponABC",
-				title: "NiponABC",
-				img: "https://crespoportfolioimages.s3.us-east-2.amazonaws.com/niponabc.webp",
-				imgAlt: "project niponabc preview image",
-				liveUrl: "https://niponabcapp.herokuapp.com/",
-				github: "https://github.com/CarlosCr18/niponABC",
-				text: "Learn and study the japanese alphabet playing",
-				skills: [
-					{
-						url: "https://crespoportfolioimages.s3.us-east-2.amazonaws.com/javascript.svg",
-						alt: "Javascript",
-					},
-					{
-						url: "https://crespoportfolioimages.s3.us-east-2.amazonaws.com/CSS3.svg",
-						alt: "CSS3",
-					},
-					{
-						url: "https://crespoportfolioimages.s3.us-east-2.amazonaws.com/react.webp",
-						alt: "React",
-					},
-					{
-						url: "https://crespoportfolioimages.s3.us-east-2.amazonaws.com/next.svg",
-						alt: "Next Js",
 					},
 				],
 			},
@@ -112,29 +158,29 @@ const Projects = () => {
 					},
 				],
 			},
-			{
-				id: "Pomodoro",
-				title: "Pomodoro App",
-				img: "https://crespoportfolioimages.s3.us-east-2.amazonaws.com/pomodoro.webp",
-				imgAlt: "project pomodoro preview image",
-				liveUrl: "https://carloscr18.github.io/pomodoro/",
-				github: "https://github.com/CarlosCr18/pomodoro",
-				text: "A clock to keep track of your sessions length",
-				skills: [
-					{
-						url: "https://crespoportfolioimages.s3.us-east-2.amazonaws.com/javascript.svg",
-						alt: "Javascript",
-					},
-					{
-						url: "https://crespoportfolioimages.s3.us-east-2.amazonaws.com/CSS3.svg",
-						alt: "CSS3",
-					},
-					{
-						url: "https://crespoportfolioimages.s3.us-east-2.amazonaws.com/react.webp",
-						alt: "React",
-					},
-				],
-			},
+			// {
+			// 	id: "Pomodoro",
+			// 	title: "Pomodoro App",
+			// 	img: "https://crespoportfolioimages.s3.us-east-2.amazonaws.com/pomodoro.webp",
+			// 	imgAlt: "project pomodoro preview image",
+			// 	liveUrl: "https://carloscr18.github.io/pomodoro/",
+			// 	github: "https://github.com/CarlosCr18/pomodoro",
+			// 	text: "A clock to keep track of your sessions length",
+			// 	skills: [
+			// 		{
+			// 			url: "https://crespoportfolioimages.s3.us-east-2.amazonaws.com/javascript.svg",
+			// 			alt: "Javascript",
+			// 		},
+			// 		{
+			// 			url: "https://crespoportfolioimages.s3.us-east-2.amazonaws.com/CSS3.svg",
+			// 			alt: "CSS3",
+			// 		},
+			// 		{
+			// 			url: "https://crespoportfolioimages.s3.us-east-2.amazonaws.com/react.webp",
+			// 			alt: "React",
+			// 		},
+			// 	],
+			// },
 		],
 	};
 	return (
